a6af82699e791e5d1714f4b85f6a9ef44ad05081
Aman1790 <97170404+Aman1790@users.noreply.github.com>
2022-05-04 21:30:32 +0530

    React from Udemy

---
+++ src/App.js
@@ -0,0 +1,13 @@
+// import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>My TODO</h1>
+      <button id="btn" >Submit</button>
+    </div>
+  );
+}
+
+export default App;
