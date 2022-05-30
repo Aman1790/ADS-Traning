3eeb1480283a5e763aa01101bd885ef283057e3d
Aman1790 <97170404+Aman1790@users.noreply.github.com>
2022-05-30 22:13:03 +0530

    React from udemy

---
+++ 05-30-2022/components/Pages/Welcome.js
@@ -0,0 +1,12 @@
+import { Route } from 'react-router-dom'
+const Welcome = () => {
+
+
+    return (
+        <div>
+            <p> Welcome to my page
+            </p>
+        </div>
+    )
+}
+export default Welcome;
